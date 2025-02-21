9eda1b13bcace16f087b8e35314e4b9b4069cf64
Joni Halabi <joni@jhalabi.com>
2025-02-21 00:47:36 -0500

    adding rss feed and fixing 404 page

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,10 +6,12 @@
   I want to be able to add HTML attributes (i.e. classes) in
   markdown. `markdown-it` will help me do that.
  */
-const markdownIt = require('markdown-it'),
-      markdownItAttrs = require('markdown-it-attrs'),
-      markdownItDeflist = require('markdown-it-deflist'),
+const markdownIt         = require('markdown-it'),
+      markdownItAttrs    = require('markdown-it-attrs'),
+      markdownItDeflist  = require('markdown-it-deflist'),
       markdownItFootnote = require('markdown-it-footnote');
+
+const { feedPlugin } = require("@11ty/eleventy-plugin-rss");
 
 
 const markdownItOptions = {
@@ -21,33 +23,40 @@ const markdownItOptions = {
 
 /* 11ty configuration exports. */
 module.exports = function(eleventyConfig) {
-  // Pass through media asset files to the built site.
+
+  /* 
+   * Pass through files to the built site.
+   */
+  // Media assets (images, etc)
   eleventyConfig.addPassthroughCopy( 'source/assets' );
 
-  // Pass through press kit documents to the built site.
-  eleventyConfig.addPassthroughCopy( 'source/press-kit/docs' );
-
-  // Pass through resume PDF to the built site.
+  // Resume PDF
   eleventyConfig.addPassthroughCopy( 'source/coding/*.pdf' );
 
-  // Pass through documents linked to from the blog to the built site.
+  // Documents linked to from the blog
   eleventyConfig.addPassthroughCopy( 'source/blog/docs/*' );
 
-  // Pass through documents linked to from the blog to the built site.
+  // Press kit documents.
   eleventyConfig.addPassthroughCopy( 'source/press-kit/docs/*' );
 
-  // Watch CSS files and automatically reload the browser if 
-  // there are changes.
+
+  /*
+   * Watch CSS files and automatically reload the browser if 
+   * there are changes.
+   */
   eleventyConfig.setBrowserSyncConfig({
 		files: './build/css/**/*.css'
 	});
 
-  // Pass the markdown library to allow me to use HTML attributes and 
-  // definition lists.
+
+  /*
+   * Configure the markdown library. This allows me to add HTML
+   * attributes, use definition lists, and create footnotes.
+   */
   let markdownLib = markdownIt( markdownItOptions ).use( markdownItAttrs ).use( markdownItDeflist ).use( markdownItFootnote );
 
-  // Customize the default opening code for the footnotes block to remove
-  // the default `hr`. 
+  // Customize the default opening code for the footnotes block to
+  // remove the default `hr`. 
   // See: https://github.com/markdown-it/markdown-it-footnote
   markdownLib.renderer.rules.footnote_block_open = () => (
     '<section class="footnotes">' +
@@ -55,6 +64,29 @@ module.exports = function(eleventyConfig) {
   );
 
   eleventyConfig.setLibrary( 'md', markdownLib );
+
+
+  /*
+   * Configure RSS feed plugin.
+   */
+  eleventyConfig.addPlugin(feedPlugin, {
+		type: "rss",
+		outputPath: "/feed.xml",
+		collection: {
+			name: "blog", // iterate over `collections.blog`
+			limit: 0,     // 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "Articles | Joni Halabi",
+			subtitle: "A blog about development, wordpress, and life as a single mother by choice.",
+			base: "https://jhalabi.com/blog",
+			author: {
+				name: "Joni Halabi",
+				email: "joni@jhalabi.com"
+			}
+		}
+	});
 };
 
 
